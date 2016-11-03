fa252dcc9fece49b97d4dd79d390365e38d7e941
mmilea <mmilea@laptop363.trading.imc.intra>
2016-11-03 10:19:13 +0100

    switching to zsh the linux dot files too

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -22,26 +22,28 @@ apt-get install python-pip
 apt-get install virtualenv
 apt-get install bash-completion
 apt-get install findutils moreutils
+apt-get install zsh
+apt-get install z
 pip install virtualenvwrapper
 pip install virtualenv
 pip install git-review
 apt-get install tree
 
-# download git-completion
-if [[ ! -r /usr/local/etc/bash_completion.d/git-completion.bash ]]; then
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o /usr/local/etc/bash_completion.d/git-completion.bash
-fi
+# switch to zsh
+chsh -s `which zsh`
 
-# download the git-prompt
-if [[ ! -r ~/.git-prompt.sh ]]; then
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-fi
+# install oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+
+# oh-my-zsh
+mkdir -p ~/.oh-my-zsh
+cd ~/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # get vundle for vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 # copy dotfiles
-cp .aliases .bash_profile .bashrc .exports .extra .functions .gitconfig .tmux.conf .vimrc ~/
+cp .aliases .bash_profile .bashrc .zshrc .exports .extra .functions .gitconfig .tmux.conf .vimrc ~/
 if [[ $? == 0 ]]; then
 	echo "Successfully copied dotfiles."
 else
